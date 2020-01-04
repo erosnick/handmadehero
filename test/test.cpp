90e47969c398ec913c5f3ffee22860a3e0347f10
psyhl5 <erosnick@gmail.com>
2020-01-05 02:48:51 +0800

    Intro to C on Windows Day 3.

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,8 @@
 #include <Windows.h>
+#include <cstdio>
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     OutputDebugString(L"Hello World!");
-
-    MessageBox(nullptr, L"Hello", L"Hello", MB_OK);
+    printf("This is a string.");
 }
