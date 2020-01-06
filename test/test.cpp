29c69e41b02cc3c1b6bbad9d4857a62a619fb8f0
psyhl5 <erosnick@gmail.com>
2020-01-06 22:48:39 +0800

    Intro to C on Windows Day 4.

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,24 @@
 #include <Windows.h>
 #include <cstdio>
 
+struct Projectile
+{
+	unsigned char IsThisOnFire;
+	int Damage;
+	int ParticlesPerSecond;
+	short HowManyCooks;
+};
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     // This this remote server editing.
     OutputDebugString(L"Hello World!");
     printf("This is a string.");
+
+    Projectile projectile;
+
+    projectile.IsThisOnFire = 1;
+    projectile.Damage = 9999;
+    projectile.ParticlesPerSecond = 100;
+    projectile.HowManyCooks = 50;
 }
