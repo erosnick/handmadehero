1d177771888ca30ff7c4bfc148bda6198d51807d
psyhl5 <erosnick@gmail.com>
2020-01-11 21:59:28 +0800

    Handmade Hero Day 002 - Opening a Win32 Window. -Add VS project.

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,8 +1,76 @@
 #include <Windows.h>
 
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParame, LPARAM LParam)
+{
+    LRESULT Result = 0;
+
+    switch (Message)
+    {
+    case WM_SIZE:
+        break;
+
+    case WM_DESTROY:
+        break;
+
+    case WM_CLOSE:
+        break;
+
+    case WM_ACTIVATEAPP:
+        break;
+
+    default:
+        Result =  DefWindowProc(Window, Message, WParame, LParam);
+        break;
+    }
+
+    return Result;
+}
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-    MessageBox(nullptr, L"Hello", L"Message", MB_OK | MB_ICONINFORMATION);
+    WNDCLASS WindowClass {};
+
+    // TODO(Princerin): Check if HDREDRAW/VREDRAW/OWNDC still matter.
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = hInstance;
+    WindowClass.lpszClassName = L"HandmadeHeroWindowClass";
+
+    if (RegisterClass(&WindowClass))
+    {
+        HWND WindowHandle = CreateWindowEx(0,                           // DWORD dwExStyle
+                            WindowClass.lpszClassName,                  // LPCSTR lpClassName
+                            L"Handmade Hero",                           // LPCSTR lpWindowName
+                            WS_OVERLAPPEDWINDOW | WS_VISIBLE,           // DWORD dwStyle
+                            CW_USEDEFAULT,                              // int X
+                            CW_USEDEFAULT,                              // int Y
+                            CW_USEDEFAULT,                              // int nWidth
+                            CW_USEDEFAULT,                              // int nHeight
+                            0,                                          // HWND hWndParent
+                            0,                                          // HMENU hMenu
+                            hInstance,                                  // HWND hInstance
+                            nullptr);                                   // LPVOID
+
+        if (WindowHandle)
+        {
+            MSG Message {};
+
+            while (Message.message != WM_QUIT)
+            {
+                BOOL MessageResult = PeekMessage(&Message, WindowHandle, 0, 0, PM_REMOVE);
+
+                if (MessageResult)
+                {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                }
+            }
+        }
+    }
+    else
+    {
+        // TODO(Princerin): Logging
+    }
 
     return 0;
 }
